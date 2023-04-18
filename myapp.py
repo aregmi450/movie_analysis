7521025e05415fbf3999005513b6c66a093868bd
Arun Regmi <aregmi450@gmail.com>
2023-04-18 08:52:33 +0545

    added visualization based on search query

--- myapp.py
+++ myapp.py
@@ -22,3 +22,24 @@ new_age_list = st.multiselect('Choose Age Range:', age_list, default=[
                               '18+', '16+', '7+', 'all'])
 # create a selectbox option that holds all unique years
 year = st.selectbox('Choose a Year', year_list, 0)
+
+# Filter the selectbox and multiselect widget for interactivity
+new_genre_year = (movieRatings['Age'].isin(new_age_list)) \
+    & (movieRatings['Year'] == year)
+
+
+# visualization
+col1, col2 = st.columns(2)
+with col1:
+    st.write("""#### Lists of movies filtered by Age Group and Release Year """)
+    dataframe_genre_year = movieRatings[new_genre_year]\
+        .groupby(['Title', 'Age'])['Year'].sum()
+    dataframe_genre_year = dataframe_genre_year.reset_index()
+    st.dataframe(dataframe_genre_year, width=400)
+
+# with col1:
+#     st.write("""#### Lists of movies filtered by year  """)
+#     year_list = movieRatings[year]\
+#         .groupby(['name', 'genre'])['year'].sum()
+#     year_list = year_list.reset_index()
+#     st.dataframe(year_list, width=400)
