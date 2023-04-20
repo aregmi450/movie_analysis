4f28269575fa91d451fa1029eea6d977e7570481
Arun Regmi <aregmi450@gmail.com>
2023-04-20 22:18:43 +0545

    added screenshot of the output

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 # importing the libraries needed
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st
