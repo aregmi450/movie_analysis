1785a1f9cae5de18f73e429f55176117c07ca6c6
Arun Regmi <aregmi450@gmail.com>
2023-04-24 09:26:04 +0545

    added pie chart and bar graph

--- myapp.py
+++ myapp.py
@@ -1,13 +1,17 @@
 # importing the libraries needed
 import pandas as pd
 import numpy as np
-# import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st
 
 # importing the dataset
 movieRatings = pd.read_csv("tv_shows.csv", index_col=[0])
+
+# filling empty datasets
+
+movieRatings.Age = movieRatings.Age.fillna('all')
 
 # Creating sidebar widget unique values from our movies dataset
 age_list = movieRatings['Age'].unique().tolist()
@@ -26,14 +30,42 @@ year = st.selectbox('Choose a Year', year_list, 0)
 new_genre_year = (movieRatings['Age'].isin(new_age_list)) \
     & (movieRatings['Year'] == year)
 
-# visualization
 
+# visualization
 st.write("""#### Lists of movies filtered by Age Group and Release Year """)
 dataframe_genre_year = movieRatings[new_genre_year]\
     .groupby(['Title', 'Age'])['Year'].sum()
 dataframe_genre_year = dataframe_genre_year.reset_index()
-st.dataframe(dataframe_genre_year, width=600)
+st.dataframe(dataframe_genre_year, width=650)
 
+
+col1, col2 = st.columns(2)
+
+# showing piechart on the dashboard
+with col1:
+    st.write("# Pie Chart Showing Shows from Different OTT")
+    movieRatings.rename(columns={"Prime Video": "PrimeVideo"}, inplace=True)
+    netflix = movieRatings['Netflix'].sum()
+    hulu = movieRatings['Hulu'].sum()
+    disney = movieRatings['Disney+'].sum()
+    prime = movieRatings['PrimeVideo'].sum()
+    labels = ['Netflix', 'Hulu', 'Disney+', 'Prime Video']
+    values = [netflix, hulu, disney, prime]
+    piechart, ax1 = plt.subplots(figsize=(10, 4))
+    ax1.pie(values, labels=labels, startangle=90)
+    ax1.axis('equal')
+    st.pyplot(piechart)
+
+
+# using seaborn to create plot
+with col2:
+    st.write("# Bar Chart Showing Age Wise Distribution of Shows")
+    fig = plt.figure(figsize=(10, 4))
+    sns.countplot(x="Age", data=movieRatings)
+    st.pyplot(fig)
+
+
+# showing top 10 rated movies in Rotten Tomatoes
 
 st.write('## Top 10 Rated Movies in Rotten Tomatoes')
 movieRatings.rename(
@@ -41,4 +73,11 @@ movieRatings.rename(
 movieRatings['RottenTomatoes'] = movieRatings['RottenTomatoes'].fillna(
     '0/100').apply(lambda x: int((x).split('/')[0]))
 top10movies = movieRatings.nlargest(10, ['RottenTomatoes'])
-st.bar_chart(x="Title", y="RottenTomatoes", data=top10movies)
+st. table(top10movies[["Title", "Year", "RottenTomatoes", "Age"]])
+
+# showing top 10 rated movies in IMDb
+st.write('## Top 10 Rated Movies in IMDB')
+movieRatings['IMDb'] = movieRatings['IMDb'].fillna(
+    '0/10').apply(lambda x: float((x).split('/')[0]))
+top10movies = movieRatings.nlargest(10, ['IMDb'])
+st. table(top10movies[["Title", "Year", "IMDb", "Age"]])
